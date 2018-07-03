d8d6c398831b71cd61eccb476652b6daccad566c
Brendon Pagano <brendonpagano@gmail.com>
2018-07-03 01:31:40 -0300

    Just render the App into the DOM

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// React
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>React Kanban</div>
+        );
+    }
+
+}
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import App from './App';
+
+
+// Render the App into the DOM.
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
