f1e68f5f1035d1eeef75238186e7fdae086addf3
Brendon Pagano <brendonpagano@gmail.com>
2018-07-03 02:25:03 -0300

    Create a constants file to classificate the types of item states

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const ITEM_STATES = {
+    TO_DO: {
+        id: 'react-kanban/TO_DO__ITEM',
+        name: 'To Do',
+    },
+    IN_PROGRESS: {
+        id: 'react-kanban/IN_PROGRESS__ITEM',
+        name: 'In Progress',
+    },
+    DONE: {
+        id: 'react-kanban/DONE__ITEM',
+        name: 'Done',
+    },
+};
